d2a0aeb8765d977a47b1213603611413ea441bb3
amirkhodjaev <amirkhodjaev5@gmail.com>
2022-08-14 15:31:40 +0500

    dumster add

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ window.onload = function () {
     document.body.classList.remove('loaded_hiding');
   }, 500);
 }
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
